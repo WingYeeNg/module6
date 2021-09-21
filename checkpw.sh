2ddc2dae3325e3f5fb78eb85302509f8cadf0fa5
WingYeeNg <wyng0@our.ecu.edu.au>
2021-09-21 09:12:10 +0800

    Add files via upload

--- checkpw.sh
+++ checkpw.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
- awk 'BEGIN { 
-      NR>1; if ( $0 !~ /[[:alnum:]]{8}[[:upper:]]+/ )
-               printf $2 " - does NOT meet passowrd strength requirements"
+ awk 'NR>1; { if ( $2 ~ /[a-zA-Z0-9]{8,}[A-Z]+[0-9]+/ )
+             {   printf $2 " -  meet password strength requirements\n"; }
             else
-               printf $2 " - meets passowrd strength requirements"
-          }' usrpwords.txt
+             {  printf $2 " - does NOT meets password strength requirements\n"; }
+            }' usrpwords.txt
 exit 0
